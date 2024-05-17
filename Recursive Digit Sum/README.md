f1f207c0db11ddf966c6e57ef88475b0dc56887f
João Matheus Del Vecchio <delvecchiobbf@gmail.com>
2024-05-17 10:20:54 -0300

    Recursive Digit Sum Problem

---
+++ Recursive Digit Sum/README.md
@@ -0,0 +1,6 @@
+Statement: https://www.hackerrank.com/challenges/one-week-preparation-kit-recursive-digit-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-four
+
+In this problem, I used recursion methods along with some specific methods, such as:
+ list() to convert a string into an array
+ map(int, n) to convert the elementos of an array n into integers
+ str() to convert a number into a string
---
+++ Recursive Digit Sum/Recursive_Digit_Sum.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+    vetor = list(map(int, n))
+    if (k*len(n) == 1):
+        return vetor[0]
+    soma = 0
+    for elemento in vetor:
+        soma +=elemento
+    soma = k*soma
+    nova_string = str(soma)
+    return superDigit(nova_string, 1)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
